c9a07e4e6d3a5a4dc8ae85e043962c7ff1188035
ABsievil <nccuong317qb@gmail.com>
2025-03-28 11:40:36 +0700

    docs: add student answer api

--- src/main/java/hcmut/examify/Controllers/RestfulAPIs/StudentController.java
+++ src/main/java/hcmut/examify/Controllers/RestfulAPIs/StudentController.java
@@ -3,11 +3,16 @@ package hcmut.examify.Controllers.RestfulAPIs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hcmut.examify.DTOs.ResponseObject;
+import hcmut.examify.DTOs.TestsDTO;
 import hcmut.examify.Services.StudentService;
 import hcmut.examify.Services.TestsService;
 
@@ -21,4 +26,27 @@ public class StudentController {
     public ResponseEntity<ResponseObject> getTestByPasscode(@RequestParam("passcode") String passcode) {
         return studentService.FNC_getTestInforByPasscode(passcode);
     }
+
+    @GetMapping("/{studentId}/answers")
+    public ResponseEntity<ResponseObject> getStudentAnswer(
+        @PathVariable("studentId") Integer studentId,
+        @RequestParam("questionId") Integer questionId ) {
+        return studentService.FNC_getStudentAnswer(studentId, questionId);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseObject> createStudentAnswer(
+        @PathVariable("studentId") Integer studentId,
+        @RequestParam("questionId") Integer questionId ){
+        return studentService.PROC_createStudentAnswer(studentId, questionId);
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseObject> updateStudentAnswer(
+        @PathVariable("studentId") Integer studentId,
+        @RequestParam("questionId") Integer questionId,
+        @RequestParam("isCorrect") Boolean isCorrect ){
+        return studentService.PROC_updateStudentAnswer(studentId, questionId, isCorrect);
+    }
+
 }
--- src/main/java/hcmut/examify/Services/StudentService.java
+++ src/main/java/hcmut/examify/Services/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -50,6 +51,87 @@ public class StudentService {
             // Xử lý các lỗi khác
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResponseObject("ERROR", "Error getting FNC_getTestInforByPasscode(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> FNC_getStudentAnswer(Integer studentId, Integer questionId) {
+        try {
+            String studentAnswers = jdbcTemplate.queryForObject(
+                    "SELECT get_studentAnswer(?, ?)",
+                    String.class, studentId, questionId
+            );
+            if (studentAnswers == null) {
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(new ResponseObject("OK", "Query to get FNC_getStudentAnswer() successfully with data = null", studentAnswers));
+            }
+
+            JsonNode jsonNode = objectMapper.readTree(studentAnswers);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseObject("OK", "Query to get FNC_getStudentAnswer() successfully", jsonNode));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (JsonProcessingException e) {
+            // Xử lý lỗi khi parse JSON
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseObject("ERROR", "JSON processing error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseObject("ERROR", "Error getting FNC_getStudentAnswer(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> PROC_createStudentAnswer(Integer studentId, Integer questionId){
+        try {
+            jdbcTemplate.execute(
+            "CALL create_studentAnswer(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setInt(1, studentId);
+                ps.setInt(2, questionId);
+ 
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_createStudentAnswer() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_createStudentAnswer(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> PROC_updateStudentAnswer(Integer studentId, Integer questionId, Boolean isCorrect){
+        try {
+            jdbcTemplate.execute(
+            "CALL update_studentAnswer(?, ?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setInt(1, studentId);
+                ps.setInt(2, questionId);
+                ps.setBoolean(3, isCorrect);
+ 
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_updateStudentAnswer() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_updateStudentAnswer(): " + e.getMessage(), null));
         }
     }
 }
