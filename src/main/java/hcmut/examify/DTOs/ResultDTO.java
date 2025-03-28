dab6643c89171837a7af49e579acc6deb3e03121
dinhduc0794 <ngdduc79@gmail.com>
2025-03-28 14:12:53 +0700

    docs: update ResultDTO

--- src/main/java/hcmut/examify/DTOs/ResultDTO.java
+++ src/main/java/hcmut/examify/DTOs/ResultDTO.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResultDTO {
-
+    private Integer studentId;
+    private Integer testId;
+    private Float totalScore;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
