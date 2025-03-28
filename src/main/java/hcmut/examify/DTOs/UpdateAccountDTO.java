d681f558fb02c5ca1a131c224a4ccaa2e548036c
ABsievil <nccuong317qb@gmail.com>
2025-03-28 16:13:04 +0700

    feat(users-api): add users api

---
+++ src/main/java/hcmut/examify/DTOs/UpdateAccountDTO.java
@@ -0,0 +1,18 @@
+package hcmut.examify.DTOs;
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateAccountDTO {
+    private Integer userId;
+    private String name;
+    private String email;
+    private Date dob;
+}
